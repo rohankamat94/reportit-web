515ff984de7047b0677f7e4a87b7dd58ba48d6ce
Rohan <rkamat30@gmail.com>
2016-02-12 22:13:04 +0530

    Add admin comment for complaint

--- src/com/cirs/jsf/controller/ViewComplaintBean.java
+++ src/com/cirs/jsf/controller/ViewComplaintBean.java
@@ -1,12 +1,20 @@
 package com.cirs.jsf.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import com.cirs.core.CIRSConstants;
 import com.cirs.dao.remote.ComplaintDao;
+import com.cirs.entities.Admin;
+import com.cirs.entities.Comment;
 import com.cirs.entities.Complaint;
 import com.cirs.entities.Complaint.ComplaintTO;
+import com.cirs.exceptions.EntityNotFoundException;
+import com.cirs.jsf.util.JsfUtils;
 
 @ViewScoped
 @ManagedBean(name = "viewComplaintBean")
@@ -17,9 +25,33 @@ public class ViewComplaintBean {
 
 	private Complaint complaint;
 	private Long complaintId;
+	private String commentData;
 
 	public void init() {
-		complaint = dao.findByIdWithComments(complaintId);		
+		Long adminId = ((Admin) JsfUtils.getExternalContext().getSessionMap().get(CIRSConstants.LOGIN_ATTRIBUTE_KEY))
+				.getId();
+		complaint = dao.findByIdWithComments(complaintId, adminId);
+
+	}
+
+	public void addComment() {
+		if (commentData == null || (commentData = commentData.trim()).equals("")) {
+			return;
+		}
+
+		System.out.println("here in add comment " + commentData);
+		Comment comment = new Comment();
+		comment.setData(commentData);
+		comment.setTime(new Timestamp(new Date().getTime()));
+		comment.setUser(null); // admin comment, null user
+		comment.setComplaint(complaint);
+		complaint.getComments().add(comment);
+		try {
+			dao.edit(complaint);
+		} catch (EntityNotFoundException e) {
+			e.printStackTrace();
+		}
+		commentData = null;
 	}
 
 	public Long getComplaintId() {
@@ -33,6 +65,14 @@ public class ViewComplaintBean {
 
 	public Complaint getComplaint() {
 		return complaint;
+	}
+
+	public String getComment() {
+		return commentData;
+	}
+
+	public void setComment(String comment) {
+		this.commentData = comment;
 	}
 
 }
